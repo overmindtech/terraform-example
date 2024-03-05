d6e8660cc81514cece36bce3e9abd207b270fbd9
jameslaneovermind <122231433+jameslaneovermind@users.noreply.github.com>
2024-03-05 15:25:15 +0000

    updated target group ARN

--- modules/scenarios/asg_change.tf
+++ modules/scenarios/asg_change.tf
@@ -21,13 +21,25 @@ resource "aws_lb_target_group" "my_target_group" {
     }
   }
 
+# Create a second Target Group
+resource "aws_lb_target_group" "my_new_target_group" {
+  name        = "asg-new-change-tg"
+  port        = 80
+  protocol    = "HTTP"
+  vpc_id      = data.aws_vpc.default.id
+
+  health_check {
+    path = "/"
+    }
+  }
+
 # Create an Auto Scaling Group
 resource "aws_autoscaling_group" "my_asg" {
   name                 = "asg-change-test-asg"
   min_size             = 0
   max_size             = 2
   desired_capacity     = 1
-  target_group_arns    = [aws_lb_target_group.my_target_group.arn]
+  target_group_arns    = [aws_lb_target_group.my_new_target_group.arn]
   availability_zones   = ["eu-west-2a"]  # Replace with your desired AZs
   health_check_type    = "EC2"
   health_check_grace_period = 300
