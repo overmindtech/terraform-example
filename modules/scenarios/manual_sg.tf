65c4f26a7ab5793dde2fa9b5369031a084a7e30e
Dylan <dylan@overmind.tech>
2024-03-05 17:09:08 +0000

    Delete old SG

--- modules/scenarios/manual_sg.tf
+++ modules/scenarios/manual_sg.tf
@@ -1,23 +1,5 @@
 # This example creates a security group and two instances, we will associate the
 # SG with the instances manually in the GUI, then try to delete it via Terraform
-resource "aws_security_group" "allow_access" {
-  name        = "allow_access"
-  description = "Allow access security group"
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
 
 resource "aws_instance" "example_1" {
   ami           = data.aws_ami.amazon_linux.id
