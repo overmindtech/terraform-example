8dc81e57ec0bbe100f1d945cfc9c7950cfcbeb64
Dylan Ratcliffe <dylan@overmind.tech>
2024-08-28 16:25:39 +0100

    Remove 8080

--- modules/scenarios/manual_sg.tf
+++ modules/scenarios/manual_sg.tf
@@ -156,12 +156,12 @@ resource "aws_instance" "app_server" {
 resource "aws_security_group" "instance_sg" {
   vpc_id = module.vpc.vpc_id
 
-  ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+  # ingress {
+  #   from_port   = 8080
+  #   to_port     = 8080
+  #   protocol    = "tcp"
+  #   cidr_blocks = ["0.0.0.0/0"]
+  # }
 
   ingress {
     from_port   = 22
