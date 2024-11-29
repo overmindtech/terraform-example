dd559e8dc1f588c1356cfbb16e2824496c952094
David Schmitt <118179693+DavidS-ovm@users.noreply.github.com>
2024-11-29 11:17:04 +0100

    Update loom.tf

--- modules/scenarios/loom.tf
+++ modules/scenarios/loom.tf
@@ -496,7 +496,7 @@ resource "aws_ecs_task_definition" "face" {
       memory    = 2048
       essential = true
       healthCheck = {
-        command  = ["CMD-SHELL", "wget -q --spider localhost:8080"]
+        command  = ["CMD-SHELL", "wget -q --spider localhost:8081"]
         interval = 30
         retries  = 3
         timeout  = 5
