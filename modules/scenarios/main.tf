652e83f3983fc153def5482e896f333b23275f2a
jameslaneovermind <122231433+jameslaneovermind@users.noreply.github.com>
2024-12-19 14:39:26 +0000

    revert to a older Amazon Linux AMI

--- modules/scenarios/main.tf
+++ modules/scenarios/main.tf
@@ -1,10 +1,10 @@
-# Get the amazon linux AMI ID
+# Get the specific Amazon Linux 2 AMI ID
 data "aws_ami" "amazon_linux" {
-  most_recent = true
+  most_recent = false
 
   filter {
     name   = "name"
-    values = ["amzn2-ami-hvm-*-x86_64-ebs"]
+    values = ["amzn2-ami-hvm-2.0.20241001.0-x86_64-ebs"]
   }
 
   filter {
