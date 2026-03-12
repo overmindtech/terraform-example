b1996825754f0caa39ff26aa4f227c038951b02b
Platform Automation <platform-automation@company.com>
2026-03-12 02:59:59 +0000

    chore: update customer IP ranges

--- main.tf
+++ main.tf
@@ -67,6 +67,11 @@ module "shared_security_group" {
 # Customer API access configuration
 locals {
   api_customer_cidrs = {
+    newco_19 = {
+      cidr = "203.0.113.119/32"
+      name = "NewCo 19"
+    }
+
     newco_18 = {
       cidr = "203.0.113.118/32"
       name = "NewCo 18"
